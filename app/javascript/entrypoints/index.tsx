1ad5407497f2100cf8cfc8420edf12cca9cd1662
Stephen McLean <stephenmclean@Stephens-MacBook-Pro.local>
2024-05-20 12:31:48 +0100

    setup react router

--- app/javascript/entrypoints/index.tsx
+++ app/javascript/entrypoints/index.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import { App } from "../components/App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
